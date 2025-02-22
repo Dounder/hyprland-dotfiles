c099610fb80615e9d5b77b133f15d08ca90b5031
dr.glasdou <dr.glasdou@gmail.com>
2025-02-21 21:30:37 -0600

    refactor: update copy script and enhance Hyprland configuration with new key bindings

--- copy.sh
+++ copy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-cp -r $HOME/.config/hypr/ $HOME/.config/waybar/ $HOME/.config/rofi/ ./.config
+hypr = $HOME/.config/hypr
+waybar = $HOME/.config/waybar
+rofi = $HOME/.config/rofi
+
+cp -r ./hypr/.config/hypr $HOME/.config
+cp -r ./waybar/.config/waybar $HOME/.config
+cp -r ./rofi/.config/rofi $HOME/.config
+
+echo "Copied hypr, waybar, and rofi to $HOME/.config"
