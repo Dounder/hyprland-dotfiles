a12ad0bf397067cb3d1a61a944423c4baa5d0861
dr.glasdou <dr.glasdou@gmail.com>
2025-02-21 21:10:35 -0600

    update Hyprland configuration and add custom aliases and git aliases

---
+++ alias.bash
@@ -0,0 +1,13 @@
+# Aliases
+alias ls='ls --color=auto'
+alias ll='ls -lah --color=auto'
+alias grep='grep --color=auto'
+alias ec="code $HOME/.zshrc" # edit .zshrc
+alias sc="source $HOME/.zshrc"  # reload zsh configuration
+alias cl='clear'
+alias gp='git push'
+alias gpp='git pull'
+alias dup='docker compose up'
+alias ddown='docker compose down'
+alias mkdir='mkdir -p'
+alias @update='npm i -g npm yarn pnpm'
---
+++ git-alias.bash
@@ -0,0 +1,18 @@
+# git log pretty
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# git status
+git config --global alias.s 'status -sb'
+# Git log --oneline
+git config --global alias.ll 'log --oneline'
+# Git last commit
+git config --global alias.last 'log -1 HEAD --stat'
+# Git diff
+git config --global alias.d 'diff'
+git config --global alias.dv 'difftool -t vimdiff -y'
+
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold  blue)<%an>%Creset' --abbrev-commit" \
+& git config --global alias.s 'status -sb' \
+& git config --global alias.ll 'log --oneline' \
+& git config --global alias.last 'log -1 HEAD --stat' \
+& git config --global alias.d 'diff' \
+& git config --global alias.dv 'difftool -t vimdiff -y'
